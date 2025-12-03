3971736ed513ba4af3290c7816665946e962515f
man1655 <manpatel1655@gmail.com>
2025-12-03 15:44:13 +0530

    react-query code

--- React-Query/src/Example.jsx
+++ React-Query/src/Example.jsx
@@ -1,8 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 function Example() {
+
+  const Details=async()=>{
+    const data= await fetch('https://jsonplaceholder.typicode.com/users')
+    const user=await data.json();
+    return user;
+  }
+  const {data,isLoading,isError}=useQuery({
+    queryKey:['users'],
+    queryFn:Details
+  })
+  if(isLoading) return <h1>Loading</h1>
+  if(isError) return <h1>Error</h1>
   return (
-    <div>Example</div>
+    <>
+   {data.map((user) => (
+        <div key={user.id}>
+          {user.name}</div>
+      ))}
+      </>
+
   )
 }
 
